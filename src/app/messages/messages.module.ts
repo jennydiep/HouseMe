5dd4defee3dac1af3b0d89d4f4464f8fca0595f9
jennydiep <jtdiep@uci.edu>
2021-05-23 18:58:21 -0700

    edited default ionic colors to match new color scheme, created tabs.

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -3,10 +3,10 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab3Page } from './tab3.page';
+import { MessagesPage } from './messages.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { Tab3PageRoutingModule } from './tab3-routing.module';
+import { MessagesPageRoutingModule } from './messages-routing.module';
 
 @NgModule({
   imports: [
@@ -14,9 +14,9 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }]),
-    Tab3PageRoutingModule,
+    RouterModule.forChild([{ path: '', component: MessagesPage }]),
+    MessagesPageRoutingModule
   ],
-  declarations: [Tab3Page]
+  declarations: [MessagesPage]
 })
-export class Tab3PageModule {}
+export class MessagesPageModule {}
