e28d0824514d801b1f0761c68290ce7b2cd31eb2
jennydiep <jtdiep@uci.edu>
2021-05-24 18:09:30 -0700

    updated input text box style, added sign up slides page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
   }
 ];
 @NgModule({
