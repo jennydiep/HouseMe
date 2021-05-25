894c333151f3c7df17429d353a45bf3bb8e70f19
jennydiep <jtdiep@uci.edu>
2021-05-25 13:18:20 -0700

    created login page

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private platform: Platform, private router : Router) { }
+
+  ngOnInit() {
+  }
+
+  navigateTo(page:string){
+    this.platform.ready().then(() => {
+      this.router.navigateByUrl(page);
+    });
+  }
+
+}
