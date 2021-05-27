9a9e699e164a0125863aed9aaaa50b227126d523
HaoRong-0118 <51035852+HaoRong-0118@users.noreply.github.com>
2021-05-27 14:27:43 -0700

    Search Page

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: 'search.page.html',
   styleUrls: ['search.page.scss']
 })
-export class SearchPage {
+export class SearchPage{
+  displaySearch:boolean
+  countResults:number
+  state:string
+  hasHousingFill:String = "outline"
+  needHousingFill:String = "solid"
+  iconButton = "arrowDown"
+  majorList = ["Computer Science","Informatics","Art","Philosophy"]
 
-  constructor() {}
 
+  search_location:string
+  search_gender:string
+  search_Major:string
+  search_rent_min:number
+  search_rent_max:number
+  search_move_in:Date
+
+  constructor() {
+    this.displaySearch = false;
+    this.countResults = 3;
+    this.state ="recommendation"
+  }
+
+  clickHasHousing(){
+    this.hasHousingFill = "solid"
+    this.needHousingFill = "outline"
+  }
+
+  clickNeedHousing(){
+    this.needHousingFill = "solid"
+    this.hasHousingFill = "outline"
+  }
+
+  showAdvanceSearch(){
+    if(this.displaySearch==false){
+      this.iconButton = "arrowUp"
+      this.displaySearch = true;
+      return
+    }
+    this.displaySearch = false;
+    this.iconButton = "arrowDown"
+  }
+  
+  applySearch(){
+    console.log("Searching:",this.search_location)
+    console.log("Searching:",this.search_gender)
+    console.log("Searching:",this.search_Major)
+    console.log("Searching:",this.search_rent_min)
+    console.log("Searching:",this.search_rent_max)
+    console.log("Searching:",this.search_move_in)
+    this.state = "search"
+  }
 }
