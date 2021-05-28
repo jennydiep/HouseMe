2999ed726684c1df34fe05a67256c3f7331fac1f
Crystal Lee <crystalleesf@gmail.com>
2021-05-27 17:29:56 -0700

    Completed first two sign up pages and functioning storage

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { StorageService } from 'src/app/storage.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,14 +8,132 @@ import {Router} from '@angular/router';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  majors: any = true;
+  isHousingClick: number = 1;
+  isLocationClick: number = 1;
+  isCoedClick: number = 1;
+  isBedroomClick: number = 1;
 
-  constructor(private router:Router) { }
+  firstName: string = "";
+  lastName: string = "";
+  birthday: string = "";
+  major:string = "";
+  gender: string = "";
+  housingStatus: string = "Have housing";
+  housingLocation: string = "On Campus";
+  peoplePerBedroom: string = "1";
+  coedPref: string = "All Genders";
+  maxRent: string = "";
+
+  userData: object = {};
+
+  constructor(private router:Router, public storageService: StorageService) {
+    this.majors = [
+      'Business Information Management',
+      'Game Design and Interactive Media',
+      'Computer Science',
+      'Computer Science and Engineering',
+      'Data Science',
+      'Informatics',
+      'Software Engineering',
+      'Undeclared'
+    ];
+  }
 
   ngOnInit() {
   }
 
   navigateToHome(){
     this.router.navigateByUrl('');
+  }
+
+  // isClicked() {
+  //   return this.isClick;
+  // }
+
+  onMaleClick() {
+    this.gender = "Male";
+  }
+
+  onFemaleClick() {
+    this.gender = "Female";
+  }
+
+  onNonBinaryClick() {
+    this.gender = "Non-Binary";
+  }
+
+  haveHousingClick() {
+    this.housingStatus = "Have housing";
+    this.isHousingClick = 1;
+  }
+
+  noHousingClick() {
+    this.housingStatus = "Do not have housing";
+    this.isHousingClick = -1;
+  }
+
+  onCampusClick() {
+    this.housingLocation = "On Campus";
+    this.isLocationClick = 1;
+  }
+
+  offCampusClick() {
+    this.housingLocation = "Off Campus";
+    this.isLocationClick = -1;
+  }
+
+  bedOneClick() {
+    this.peoplePerBedroom = "1";
+    this.isBedroomClick = 1;
+  }
+
+  bedTwoClick() {
+    this.peoplePerBedroom = "2";
+    this.isBedroomClick = 2;
+  }
+  
+  bedThreeClick() {
+    this.peoplePerBedroom = "3";
+    this.isBedroomClick = 3;
+  }
+
+  bedFourClick() {
+    this.peoplePerBedroom = "4";
+    this.isBedroomClick = 4;
+  }
+
+  yesCoedClick() {
+    this.coedPref = "All Genders";
+    this.isCoedClick = 1;
+  }
+
+  noCoedClick() {
+    this.coedPref = "Similar Only";
+    this.isCoedClick = -1;
+  }
+
+  addUserData() {
+    this.userData["firstName"] = this.firstName;
+    this.userData["lastName"] = this.lastName;
+    this.userData["birthday"] = this.birthday;
+    this.userData["major"] = this.major;
+    this.userData["gender"] = this.gender;
+    this.userData["housingStatus"] = this.housingStatus;
+    this.userData["housingLocation"] = this.housingLocation;
+    this.userData["peoplePerBedroom"] = this.peoplePerBedroom;
+    this.userData["coedPref"] = this.coedPref;
+    this.userData["maxRent"] = this.maxRent;
+
+
+  //   this.storageService.addUser(this.firstName, this.userData);
+  //   this.storageService.getUser(this.firstName).then(res => {
+  //     if (res != null) {
+  //       console.log(res);
+  //     }
+  //   }).catch(e => {
+  //     console.log('error: ', e);
+  //   });
   }
 
 }
---
+++ src/app/storage.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  constructor(public storage: Storage) {
+    console.log('Storage provider is working');
+    this.init();
+  }
+
+  async init() {
+    const storage = await this.storage.create();
+    this.storage = storage;
+  }
+
+  // Create and expose methods that users of this service can call
+  async addUser(key: string, object: Object) {
+    try {
+      const result = await this.storage.set(key, JSON.stringify(object));
+      console.log('set Object in storage: ' + result);
+      return true;
+    }
+    catch (reason) {
+      console.log(reason);
+      return false;
+    }
+  }
+
+  async getUser(key: string): Promise<any> {
+    try {
+      const result = await this.storage.get(key);
+      if (result != null) {
+        return JSON.parse(result);
+      }
+      return null;
+    }
+    catch (reason) {
+      console.log(reason);
+      return null;
+    }
+  }
+
+  remove(key: string) {
+    this.storage.remove(key);
+  }
+
+  clear() {
+    this.storage.clear();
+  }
+}
