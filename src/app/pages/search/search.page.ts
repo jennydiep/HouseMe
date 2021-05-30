4817f801bd509f57ef2a682091d798c65cb25b4f
HaoRong-0118 <51035852+HaoRong-0118@users.noreply.github.com>
2021-05-29 22:23:43 -0700

    Card Structure on Search Page

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,3 +1,8 @@
+//TODO: Handle Rent Range Input to be only numbers
+//TODO: Handle 
+//TODO: Has Housing shall has a different set of questions
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,11 +14,12 @@ export class SearchPage{
   displaySearch:boolean
   countResults:number
   state:string
-  hasHousingFill:String = "outline"
-  needHousingFill:String = "solid"
+  hasHousingFill:String = "solid"
+  needHousingFill:String = "outline"
   iconButton = "arrowDown"
   majorList = ["Computer Science","Informatics","Art","Philosophy"]
-
+  recommendationList =[]
+  personList=[]
 
   search_location:string
   search_gender:string
@@ -24,18 +30,50 @@ export class SearchPage{
 
   constructor() {
     this.displaySearch = false;
-    this.countResults = 3;
     this.state ="recommendation"
+    
+    
+    let person1 = {firstName: "Hao",lastName:"Rong",housingStatus:"Has",major:"Valorant",bio:"Hello, I am new to the area and would like to live with people with similar interests such as coding",img:""}
+    let person2 = {firstName: "Jenny",lastName:"Diep",housingStatus:"Has",major:"Apex",bio:"Hello, I am new to the area and would like to live with people with similar interests such as coding"}
+    let person3 = {firstName: "Kaeley",lastName:"Lenard",housingStatus:"Need",major:"CS",bio:"Hello, I am new to the area and would like to live with people with similar interests such as coding"}
+    let person4 = {firstName: "Crystal",lastName:"Lee",housingStatus:"Need",major:"Informatics",bio:"Hello, I am new to the area and would like to live with people with similar interests such as coding"}
+    this.personList.push(person1)
+    this.personList.push(person2)
+    this.personList.push(person3)
+    this.personList.push(person4)
+
+    for(let i = 0 ; i<this.personList.length; i++){
+      if(this.personList[i]["housingStatus"]=="Has"){
+        this.recommendationList.push(this.personList[i]);
+      }
+    }
+    this.countResults =this.recommendationList.length;
   }
 
   clickHasHousing(){
     this.hasHousingFill = "solid"
     this.needHousingFill = "outline"
+
+    this.recommendationList=[]
+    for(let i = 0 ; i<this.personList.length; i++){
+      if(this.personList[i]["housingStatus"]=="Has"){
+        this.recommendationList.push(this.personList[i]);
+      }
+    }
+    this.countResults =this.recommendationList.length;
   }
 
   clickNeedHousing(){
     this.needHousingFill = "solid"
     this.hasHousingFill = "outline"
+
+    this.recommendationList=[]
+    for(let i = 0 ; i<this.personList.length; i++){
+      if(this.personList[i]["housingStatus"]=="Need"){
+        this.recommendationList.push(this.personList[i]);
+      }
+    }
+    this.countResults =this.recommendationList.length;
   }
 
   showAdvanceSearch(){
