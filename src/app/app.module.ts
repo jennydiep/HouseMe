3fb7d977ae56948484f046da819493a0ac1a1f12
LeoYoungg <57172043+LeoYoungg@users.noreply.github.com>
2021-06-01 10:26:02 -0700

    implemeted messages page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { IonicStorageModule } from '@ionic/storage-angular';
+// import { IonicStorageModule } from '@ionic/storage';
+import { Storage } from '@ionic/storage';
+// import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy},Storage],
   bootstrap: [AppComponent],
+
 })
 export class AppModule {}
